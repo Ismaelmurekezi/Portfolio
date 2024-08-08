ea13fe06662dc009c21b0570db24aae603955659
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2024-08-08 20:46:57 +0200

    setting up project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import Project from "./components/Project";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
